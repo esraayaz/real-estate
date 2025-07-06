edad982a00b0759cc0af0edcff18c4ddb513c033
Esra Ayaz <essayaz@hotmail.com>
2025-07-07 00:29:08 +0200

    Project is on!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./Components/Header";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
