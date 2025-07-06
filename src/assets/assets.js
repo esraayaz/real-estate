e6f1d2ffc8439cb22d2ae0a0ef9606d14522b998
Esra Ayaz <essayaz@hotmail.com>
2025-07-07 00:28:36 +0200

    Assets fixed

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -13,9 +13,9 @@ import project_img_3 from "./project_img_3.jpg";
 import project_img_4 from "./project_img_4.jpg";
 import project_img_5 from "./project_img_5.jpg";
 import project_img_6 from "./project_img_6.jpg";
-import profile_img_1 from "./profile_img_1.png";
-import profile_img_2 from "./profile_img_2.png";
-import profile_img_3 from "./profile_img_3.png";
+import profile_img_1 from "./profile_img_1.jpg";
+import profile_img_2 from "./profile_img_2.jpg";
+import profile_img_3 from "./profile_img_3.jpg";
 
 export const assets = {
   logo,
