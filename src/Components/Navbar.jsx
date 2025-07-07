3fb4b1366fe8c9a936a90beb62a7630254296340
Esra Ayaz <essayaz@hotmail.com>
2025-07-07 02:09:08 +0200

    Gap issue fixed.

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
     <div className="absolute top-0 left-0 w-full z-10">
       <div className="container mx-auto flex justify-between items-center py-4 px-6 md:px-20 lg:px-32 bg:transparent">
         <img src={assets.logo} alt="logo" className="w-30" />
-        <ul className="hidden md:flex gap-7 text-white">
+        <ul className="text-shadow-md lg:gap-20 hidden md:flex gap-7 text-white">
           <a href="#Header" className="cursor-pointer hover:text-black">
             Home
           </a>
