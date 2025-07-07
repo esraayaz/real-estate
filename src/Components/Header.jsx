2457f8953b2fdad0d4f6b1560677704d67dbda08
Esra Ayaz <essayaz@hotmail.com>
2025-07-07 21:03:13 +0200

    Title changed.

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
       <Navbar />
       <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
         <h2 className="text-shadow-md text-5xl sm:text-6xl md:text.[82px] inline-block max-w-3xl font-semibold pt-20">
-          Houses of dreams.
+          Timeless Spaces
         </h2>
         <div className="space-x-6 mt-16">
           <a
