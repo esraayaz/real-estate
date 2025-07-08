394d2e3d03cd3520a7f922c7dcb103243313041d
Esra Ayaz <essayaz@hotmail.com>
2025-07-08 23:22:18 +0200

    Testimonials section created 🌱

---
+++ src/Components/Testimonials.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Testimonials = () => {
+  return (
+    <div
+      className="container mx-auto py-10 lg:px-32 2-full overflow-hidden"
+      id="Testimonials"
+    >
+      <h1>
+        Customer <span>Testimonials</span>
+      </h1>
+      <p>Hear from our investors about their exclusive property journey.</p>
+    </div>
+  );
+};
+
+export default Testimonials;
