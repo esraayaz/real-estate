177d962e9f2fa934de6201818de6a1f926d8da90
Esra Ayaz <essayaz@hotmail.com>
2025-07-09 15:25:06 +0200

    Testimonials component updated 🌷

--- src/Components/Testimonials.jsx
+++ src/Components/Testimonials.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { testimonialsData, assets } from "../assets/assets";
 
 const Testimonials = () => {
   return (
@@ -6,10 +7,42 @@ const Testimonials = () => {
       className="container mx-auto py-10 lg:px-32 2-full overflow-hidden"
       id="Testimonials"
     >
-      <h1>
-        Customer <span>Testimonials</span>
+      <h1 className="text-2xl sm:text-4xl font-bold mb-2 text-center">
+        Our Customers
       </h1>
-      <p>Hear from our investors about their exclusive property journey.</p>
+      <p className="text-center text-gray-500 mb-12 max-w-80 mx-auto">
+        Hear from our investors about their exclusive property journey.
+      </p>
+
+      <div className="flex flex-wrap justify-center gap-8">
+        {testimonialsData.map((testimonial, index) => (
+          <div
+            key={index}
+            className="max-w-[340px] border border-gray-300 shadow-lg rounded px-8 py-12 text-center"
+          >
+            <img
+              className="w-40 h-40 rounded-full mx-auto mb-4"
+              src={testimonial.image}
+              alt={testimonial.alt}
+            />
+            <h2 className="text-xl text-gray-700 font-medium">
+              {testimonial.name}
+            </h2>
+            <p className="text-gray-500 mb-4 text-sm">{testimonial.title}</p>
+            <div className="flex justify-center gap-1 text-red-500 mb-4">
+              {Array.from({ length: testimonial.rating }, (item, index) => (
+                <img
+                  className="w-4 h-4"
+                  key={index}
+                  src={assets.star_icon}
+                  alt="star-icon"
+                />
+              ))}
+            </div>
+            <p className="text-grap-600">{testimonial.text}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
